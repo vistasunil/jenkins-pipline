be0021c922eeb6d70eb4bae482fcf3d231ad94fa
vistasunil <37858762+vistasunil@users.noreply.github.com>
2021-06-15 01:27:37 +0530

    Update Selenium.java

--- src/main/java/test/Selenium.java
+++ src/main/java/test/Selenium.java
@@ -1,1 +1,22 @@
-ff
+package com.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+   public static void main(String[] args) {
+       System.setProperty("webdriver.chrome.driver","chromedriver");	// <-- Change this path
+       WebDriver driver = new ChromeDriver();
+       String baseUrl = "https://experitest.com/free-trial/";
+       String expectedTitle = "Free trial";
+       String actualTitle = "";
+       driver.get(baseUrl);
+       actualTitle = driver.getTitle();
+       if (actualTitle.contentEquals(expectedTitle)){
+           System.out.println("TEST PASSED!");
+       } else {
+           System.out.println("TEST FAILED");
+       }
+       driver.close();
+   }
+}
